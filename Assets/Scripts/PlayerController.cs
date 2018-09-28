53a6c15c204cb834263ce2581fa72c0a2743f29c
Jacky <jackymayonaise@gmail.com>
2018-09-28 17:50:33 -0400

    Add to allow transfer to Trottier computers

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    public int numOfPickUps;
+    private Rigidbody rb;
+    private int count;
+    private IList<GameObject> listOfInactivePickUps = new List<GameObject>();
+    void RestartGame()
+    {
+        // All text transformations
+        count = 0;
+        SetCountText();
+        winText.enabled = false;
+        // All player transformations
+        this.transform.position = Vector3.zero;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        for (int i = 0; i < listOfInactivePickUps.Count; i++){
+            listOfInactivePickUps[i].SetActive(true);
+        }
+    }
+
+    void Start()
+    {
+        count = 0;
+        rb = GetComponent<Rigidbody>();
+        SetCountText();
+        winText.text = "You Win!";
+        winText.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp("r"))
+        {
+
+            RestartGame();
+
+        }
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            listOfInactivePickUps.Add(other.gameObject);
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+        if (count >= numOfPickUps)
+        {
+            winText.enabled = true;
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    System.Random randomGen;
+    private Vector3 randomRotationVector;
+    private void Start()
+    {        
+        // Given boundary of X is from -10 to 10, we can use a range of -21
+        randomGen = new System.Random((int)(this.gameObject.transform.position.x* 214748));
+        randomRotationVector = new Vector3(randomGen.Next(0, 90), randomGen.Next(0, 90), randomGen.Next(0, 90));
+
+
+    }
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(randomRotationVector * Time.deltaTime);
+	}
+}
